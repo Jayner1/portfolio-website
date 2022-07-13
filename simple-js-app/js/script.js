54f1a723cd5ca620c3e6e1e55cc23266d64920c1
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2022-07-13 11:06:05 -0700

    added IIFE and functions.

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,20 +1,35 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
 //My 6 Starters always when playing pokemon growing up
   {name:'Charizard', height:5.6, type:['fire', 'flying']},
   {name:'Alakazam', height:4.9, type:['psychic']},
   {name:'Dragonite', height:7.2, type:['dragon', 'flying']},
-  {name:'Scyther', height:4.9, type:['bug', 'flying']}.
+  {name:'Scyther', height:4.9, type:['bug', 'flying']},
   {name:'Gyarados', height:21.3, type:['water', 'flying']},
   {name:'Jolteon', height:2.6, type:['electric']},
 //Honorable mentions:gengar, lapras, articuno, zapdos,
 //mewtwo, raichu, pidgeot.
 ];
+	function getAll() {
+    	return pokemonList;
+   }
+
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+   }
 
 //working on the for part. Feel confused about it.
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height <8 && pokemonList[i].height >0){
-    document.write(pokemonList[i].height + " is a short Pokemon");
-  }else if pokemonList[i].height >8){
-    document.write(pokemonList[i].height + " is a tall Pokemon");
-  }
-}
+// for (let i=0; i < pokemonList.length; i++){
+//   if (pokemonList[i].height <8 && pokemonList[i].height >0){
+//     document.write(pokemonList[i].height + " is a short Pokemon");
+//   }else if pokemonList[i].height >8){
+//     document.write(pokemonList[i].height + " is a tall Pokemon");
+//   }
+// }
+//
+// pokemonList.forEach(function() {
+//   console.log(.name + ' is ' + .height ' which is a short or tall pokemon.');
+//   });
+
+pokemonList.forEach( name => console.log(name) );
+})();
